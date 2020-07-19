43294de37af52097bbb8c28a2d86deec40a5ee7f
nehagiribhat <nehagiribhat@gmail.com>
2020-07-19 23:30:33 +0530

    ADDED: Echoing Number of Heads and Tails

--- flipCoin.sh
+++ flipCoin.sh
@@ -1,10 +1,22 @@
 #!/bin/bash -x
 
-outcome=$((RANDOM%2))
+#variables
+flip=0
+total_no_of_heads=0
+total_no_of_tails=0
 
-if [ $outcome -eq 1 ]
-then
-	echo "Heads Won"
-else
-	echo "Tails won"
-fi
+read -p "Enter number of times to flip: " limit
+while [ $flip -lt $limit ]
+do
+	outcome=$((RANDOM%2))
+	if [ $outcome -eq 1 ]
+	then
+		((total_no_of_heads++))
+	else
+		((total_no_of_tails++))
+	fi
+	((flip++))
+done
+
+echo "Heads occured $total_no_of_heads times"
+echo "Tails occured $total_no_of_tails times"
