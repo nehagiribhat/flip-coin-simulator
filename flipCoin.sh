b47b1b501ae99c47ad1b135b861a4acdf9907fad
nehagiribhat <nehagiribhat@gmail.com>
2020-07-19 23:45:09 +0530

    REFACTOR: Flipping Coin Till a Difference of 2 is Reached

--- flipCoin.sh
+++ flipCoin.sh
@@ -4,18 +4,19 @@
 flip=0
 total_no_of_heads=0
 total_no_of_tails=0
+limit=21
 
-read -p "Enter number of times to flip: " limit
 while [ $flip -lt $limit ]
 do
-        outcome=$((RANDOM%2))
-        if [ $outcome -eq 1 ]
-        then
-                ((total_no_of_heads++))
-        else
-                ((total_no_of_tails++))
-        fi
-        ((flip++))
+	outcome=$((RANDOM%2))
+	if [ $outcome -eq 1 ]
+	then
+		((flip++))
+		total_no_of_heads=$flip
+	else
+		((flip++))
+		total_no_of_tails=$flip
+	fi
 done
 
 echo "Heads occured $total_no_of_heads times"
@@ -24,13 +25,23 @@ echo "Tails occured $total_no_of_tails times"
 if [ $total_no_of_heads -eq $total_no_of_tails ]
 then
 	echo "It's a TIE"
-else
-	if [ $total_no_of_heads -eq $total_no_of_tails ]
+	difference1=$((total_no_of_heads-total_no_of_tails))
+	difference2=$((total_no_of_tails-total_no_of_heads))
+	if [ $difference1 -ge 2 -o $difference2 -ge 2 ]
 	then
-		difference=$((total_no_of_heads-total_no_of_tails))
-		echo "Heads Wins by $difference"
+		if [ $total_no_of_heads -gt $total_no_of_tails ]
+		then
+        		difference=$((total_no_of_heads-total_no_of_tails))
+        		echo "Heads Wins by $difference"
+		else
+        		difference=$((total_no_of_tails-total_no_of_heads))
+        		echo "Tails Wins by $difference"
+		fi
+		((limit++))
 	else
-		difference=$((total_no_of_tails-total_no_of_heads))
-                echo "Tails Wins by $difference"
+		echo "Minimum Difference NOT Reached"
+		break
 	fi
+else
+	echo "No TIE Occured"
 fi
